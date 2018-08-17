02702719b5c05e7d5442928a1e472fdb8b5dee99
Sean Rice <seanrice2015@gmail.com>
2018-08-17 15:34:57 -0400

    Shortened code and made UX better

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ import glob
 import shutil
 import errno
 
+
 HOME= os.path.expanduser('~')
 SOURCE_DOTS = HOME + '/dotfiles/dots'
 SOURCE_SUBS = HOME + '/dotfiles/subs'
+SOURCES = [SOURCE_DOTS, SOURCE_SUBS]
 DOT_ARCHIVE = HOME + '/.dotArchive/'
 EXCLUDE = [".git", ".gitignore", ".gitmodules"]
 NO_DOT_PREFIX = []
@@ -27,6 +29,7 @@ def is_link_to(link, dest):
     is_link = is_link and os.readlink(link).rstrip('/') == dest.rstrip('/')
     return is_link
 
+
 def archive(dest):
     try:
         os.mkdir(DOT_ARCHIVE)
@@ -34,73 +37,51 @@ def archive(dest):
         if exc.errno != errno.EEXIST:
             raise
         pass
+    
     print "Archiving %s to %s" % (dest, DOT_ARCHIVE + os.path.basename(dest))
-
     shutil.move(dest, DOT_ARCHIVE + os.path.basename(dest))
 
 
-def symlink_dots():
-    os.chdir(os.path.expanduser(SOURCE_DOTS))
-    for filename in [file for file in glob.glob('*') if file not in EXCLUDE]:
-        dotfile = filename
-        if filename not in NO_DOT_PREFIX:
-            dotfile = '.' + dotfile
-        if filename not in PRESERVE_EXTENSION:
-            dotfile = os.path.splitext(dotfile)[0]
-        dotfile = os.path.join(os.path.expanduser('~'), dotfile)
-        source = os.path.join(SOURCE_DOTS, filename).replace('~', '.')
-        # Check that we aren't overwriting anything
-        if os.path.lexists(dotfile):
-            
-            if os.path.islink(dotfile):
-                if is_link_to(dotfile, source):
-                    continue
-
-                response = raw_input("Overwrite file `%s'? [y/N] " % dotfile)
-                if not response.lower().startswith('y'):
-                    print "Skipping `%s'..." % dotfile
-                    continue
-
-                force_remove(dotfile)
-            else:
-                archive(dotfile)
-
-        os.symlink(source, dotfile)
-        print "%s => %s" % (dotfile, source)
-
-def symlink_subs():
-    os.chdir(os.path.expanduser(SOURCE_SUBS))
-    for filename in [file for file in glob.glob('*') if file not in EXCLUDE]:
-        dotfile = filename
-        if filename not in NO_DOT_PREFIX:
-            dotfile = '.' + dotfile
-        if filename not in PRESERVE_EXTENSION:
-            dotfile = os.path.splitext(dotfile)[0]
-        dotfile = os.path.join(os.path.expanduser('~'), dotfile)
-        source = os.path.join(SOURCE_SUBS, filename).replace('~', '.')
-        # Check that we aren't overwriting anything
-        if os.path.lexists(dotfile):
-            
-            if os.path.islink(dotfile):
-                if is_link_to(dotfile, source):
-                    continue
-
-                response = raw_input("Overwrite file `%s'? [y/N] " % dotfile)
-                if not response.lower().startswith('y'):
-                    print "Skipping `%s'..." % dotfile
-                    continue
-
-                force_remove(dotfile)
-            else:
-                archive(dotfile)
-
-        os.symlink(source, dotfile)
-        print "%s => %s" % (dotfile, source)
-
-
 def main():
-    symlink_dots()
-    symlink_subs()
+    for source_dir in SOURCES:
+        if source_dir == SOURCE_DOTS:
+            print "Linking dotfiles to ~/ ..."
+        else: 
+            print "Linking submodules to ~/ ..."
+
+        os.chdir(os.path.expanduser(source_dir))
+        for filename in [file for file in glob.glob('*') if file not in EXCLUDE]:
+            dotfile = filename
+            if filename not in NO_DOT_PREFIX:
+                dotfile = '.' + dotfile
+            if filename not in PRESERVE_EXTENSION:
+                dotfile = os.path.splitext(dotfile)[0]
+            dotfile = os.path.join(os.path.expanduser('~'), dotfile)
+            source = os.path.join(source_dir, filename).replace('~', '.')
+
+            # Check that we aren't overwriting anything
+            if os.path.lexists(dotfile):
+                
+                if os.path.islink(dotfile):
+                    if is_link_to(dotfile, source):
+                        continue
+
+                    response = raw_input("Overwrite file `%s'? [y/N] " % dotfile)
+                    if not response.lower().startswith('y'):
+                        print "Skipping `%s'..." % dotfile
+                        continue
+
+                    force_remove(dotfile)
+                else:
+                    response = raw_input("Archive `%s'? [y/N] " % dotfile)
+                    if not response.lower().startswith('y'):
+                        print "Skipping `%s'..." % dotfile
+                        continue
+
+                    archive(dotfile)
+
+            os.symlink(source, dotfile)
+            print "%s => %s" % (dotfile, source)
 
 if __name__ == '__main__':
     main()
