8dfd7853a31922359319a1ae063efe0ce309c32e
Sean Rice <seanrice2015@gmail.com>
2018-08-24 14:14:17 -0400

    Added unlink and setup options

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import os
 import glob
 import shutil
 import errno
+import subprocess
 
 
 HOME= os.path.expanduser('~')
@@ -42,7 +43,7 @@ def archive(dest):
     shutil.move(dest, DOT_ARCHIVE + os.path.basename(dest))
 
 
-def main():
+def  link():
     for source_dir in SOURCES:
         if source_dir == SOURCE_DOTS:
             print "Linking dotfiles to ~/ ..."
@@ -75,13 +76,61 @@ def main():
                 else:
                     response = raw_input("Archive `%s'? [y/N] " % dotfile)
                     if not response.lower().startswith('y'):
-                        print "Skipping `%s'..." % dotfile
+                        print "Archiving `%s'..." % dotfile
                         continue
 
                     archive(dotfile)
 
             os.symlink(source, dotfile)
             print "%s => %s" % (dotfile, source)
+
+def unlink():
+    for source_dir in SOURCES:
+        if source_dir == SOURCE_DOTS:
+            print "Removing symlinks to dotfiles in ~/ ..."
+        else: 
+            print "Removing symlinks to submodules in ~/ ..."
+
+        os.chdir(os.path.expanduser(source_dir))
+        for filename in [file for file in glob.glob('*') if file not in EXCLUDE]:
+            dotfile = filename
+            if filename not in NO_DOT_PREFIX:
+                dotfile = '.' + dotfile
+            if filename not in PRESERVE_EXTENSION:
+                dotfile = os.path.splitext(dotfile)[0]
+            dotfile = os.path.join(os.path.expanduser('~'), dotfile)
+            source = os.path.join(source_dir, filename).replace('~', '.')
+
+            if os.path.lexists(dotfile):
+                if os.path.islink(dotfile):
+                    if is_link_to(dotfile, source):
+                        force_remove(dotfile)
+
+
+def setup():
+    subprocess.Popen("chsh -s $(which zsh)")
+    subprocess.Popen("vim +PluginInstall +qall")
+    print("Logout and log back in for changes to take effect!")
+    
+
+def main():
+    print("link - make sym links within ~/ for all dotfiles and submodules, pre appending a dot.")
+    print("unlink - remove all symlinks from ~/ to any resources contained in this directory.")
+    print("setup - run script to set up dotfile requirements and dependencies.")
+    print("exit - exit the dotfile helper")
+    response = raw_input("What would you like to do? \n")
+
+    while 1:
+        if response.lower() == "exit":
+            break
+        elif response.lower() == "setup":
+            setup()
+        elif response.lower() == "link":
+            link()
+        else:
+            print("Valid options are link, unlink, setup and exit")
+
+        response = raw_input("Would you like to do anything else? If not, enter exit \n")
 
 if __name__ == '__main__':
     main()
