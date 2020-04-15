1bdf828e974ba94833cde1dbf8490f1cdbaaf54a
Sean Rice <seanrice2015@gmail.com>
2020-04-15 16:53:56 -0700

    Removed a few things, added new config

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ import subprocess
 
 
 HOME= os.path.expanduser('~')
+HOST_OS = None
 SOURCE_DOTS = HOME + '/dotfiles/dots'
 SOURCE_SUBS = HOME + '/dotfiles/subs'
 SOURCES = [SOURCE_DOTS, SOURCE_SUBS]
@@ -108,32 +109,48 @@ def unlink():
 
 
 def setup():
-    subprocess.call("chsh -s $(which zsh)", shell=True)
-    subprocess.call("vim +PluginInstall +qall", shell=True)
+    link();
+
+    # Test for vim clipboard 
+    if (subprocess.run("vim --version | grep '+clipboards;'", shell=True)).returncode == 1:
+        print("To enable clipboard copy and paste within vim, you must have the +clipboards flag enabled within vim")
+
+    subprocess.run("chsh -s $(which zsh)", shell=True, check=True)
+    subprocess.run("vim +PluginInstall +qall", shell=True, check=True)
     print("Logout and log back in for changes to take effect!")
     
 
 def main():
     print("======================== Welcome to the dotfile configure-er ========================")
     print("Commands: ")
-    print("link - make sym links within ~/ for all dotfiles and submodules, pre appending a dot.")
+    print("setup - make sym links within ~/ for all dotfiles and submodules, pre appending a dot. Run setup scripts.")
     print("unlink - remove all symlinks from ~/ to any resources contained in this directory.")
-    print("setup - run script to set up dotfile requirements and dependencies.")
     print("exit - exit the dotfile helper")
     print("=====================================================================================")
+
+    print("OS types: macOS, linux, windows"
+    response = raw_input("Set OS \n")
+
+    if  response.lower() == "windows":
+        print("Sorry windows is not supported yet. Quitting...")
+        sys.exit(0)
+    elif response.lower() != "macos" or response.lower() != "linux":
+        print("Unsupported OS. Qutting...")
+        sys.exit(1)
+    else:
+        HOST_OS=response.lower()
+
     response = raw_input("What would you like to do? \n")
 
     while 1:
         if response.lower() == "exit":
-            break
+            sys.exit(1)
         elif response.lower() == "setup":
             setup()
-        elif response.lower() == "link":
-            link()
         elif response.lower() == "unlink":
             unlink()
         else:
-            print("Valid options are link, unlink, setup and exit")
+            print("Valid options are unlink, setup and exit")
 
         response = raw_input("Would you like to do anything else? If not, enter exit \n")
 
